2c6b6ad59865363b634c960b1edd154677f8c2c7
Anand Prabhu <anandprab@gmail.com>
2021-05-09 22:32:25 +0530

    ENd of part 18

--- routes/admin.js
+++ routes/admin.js
@@ -4,33 +4,10 @@ var productHelpers=require("../helpers/product-helpers")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-      name:"OnePlus 8T",
-      category:"Mobile",
-      description:"Aquamarine Green",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfzKBGZfVW9NLY1uGYtVCToA7qY7_o4ej23ZIr9hxk_7c5FmvXr2HjjwVVokU&usqp=CAc"
-    },
-    {
-      name:"Samsung Galaxy M31",
-      category:"Mobile",
-      description:"Ocean blue",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWL2V7vQUsuFBM3_zUkoMxuExqpOVBUuU2DAAiUsdz80EXnFeF_4a0gc7nkw&usqp=CAc"
-    },
-    {
-      name:"iPhone 12",
-      category:"Mobile",
-      description:"Black",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpkcCFMD6Z4QB0A5QYrHaOWwscC4BJxi2jhtXFRxgi5zX9BIfmOpKHjksb0PL-wuVD_5BswMiA&usqp=CAchttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpkcCFMD6Z4QB0A5QYrHaOWwscC4BJxi2jhtXFRxgi5zX9BIfmOpKHjksb0PL-wuVD_5BswMiA&usqp=CAchttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfzKBGZfVW9NLY1uGYtVCToA7qY7_o4ej23ZIr9hxk_7c5FmvXr2HjjwVVokU&usqp=CAc"
-    },
-    {
-      name:"Redmi Note 10",
-      category:"Mobile",
-      description:"Black",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyF7Qw0CEvWyeoVcmEIeow_fpSXNWjgvTeF827SQZKfBeK9pqf8Yb4KL8M8dJWLArW3K3EEOQ&usqp=CAchttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfzKBGZfVW9NLY1uGYtVCToA7qY7_o4ej23ZIr9hxk_7c5FmvXr2HjjwVVokU&usqp=CAc"
-    }
-  ]
-  res.render("admin/view-products", {admin:true,products});
+ productHelpers.getAllProducts().then((products)=>{
+   console.log(products);
+res.render("admin/view-products", {admin:true, products})
+ })
 });
 
 router.get("/add-product", function(req,res){
