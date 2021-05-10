640fd7878ac38ebdcd9c3cbc1f25710489f9bd41
Anand Prabhu <anandprab@gmail.com>
2021-05-10 14:14:52 +0530

    End of part 19

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var hbs=require('express-handlebars')
 var fileUpload=require('express-fileupload')
 var db=require("./config/connection")
+var session=require("express-session")
+
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 
@@ -21,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+app.use(session({secret:"Key",cookie:{maxAge:600000}}))
 
 db.connect((err)=>{
   if(err) console.log("connection error "+err);
